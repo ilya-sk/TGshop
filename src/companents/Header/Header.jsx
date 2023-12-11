bffae16566ea93f6d6fa99789196c3c767b3db02
Илья С <ilas@192.168.1.8>
2023-12-11 18:29:22 +0200

    add header

--- src/companents/Header/Header.jsx
+++ src/companents/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
 
     return (
         <div className={'header'}>
-            <Button onClick={onClose}>Закрыть</Button>
+            Добро пожаловать:
             <span className={'username'}>
                 {user?.username}
             </span>
