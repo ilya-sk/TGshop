3b245c98a3922d3cb0df2e0d1027cc4f9b585092
Илья С <ilas@192.168.1.8>
2023-12-12 16:35:26 +0200

    add form

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import "./App.css";
 import ProductList from "./companents/ProdList/ProductList";
 import Header from "./companents/Header/Header";
 
-const tg = window.Telegram.WebApp;
+// const tg = window.Telegram.WebApp;
 
 function App() {
+  const {onToggleButton, tg} = useTelegram();
+
   useEffect(() => {
-    tg.ready();
-  }, []);
-  const onClose = () => {
-    tg.close();
-  };
+      tg.ready();
+  }, [])
 
   return (
-    <div className="App">
-      <Header/>
-      <ProductList/>
-    </div>
+      <div className="App">
+          <Header />
+          <Routes>
+              <Route index element={<ProductList />}/>
+              <Route path={'form'} element={<Form />}/>
+          </Routes>
+      </div>
   );
 }
 
