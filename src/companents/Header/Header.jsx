094ba822df52a535a7418b0fe3ff6e29c6b42dce
Илья С <skachilya@gmail.com>
2024-05-01 21:54:32 +0300

    ver.beta 1.0  safari

--- src/companents/Header/Header.jsx
+++ src/companents/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
 import './Header.css';
-import logo from './logo.jpg'
+import logo from './logo.png'
 
 const Header = () => {
     const {user, onClose} = useTelegram();
